e3573e4c57a7ad20a3601cd34c421e167ab0e7f0
harunugurlu <harunugurlu1@gmail.com>
2024-05-12 12:18:07 +0300

    add redirection notice

--- background.js
+++ background.js
@@ -1,15 +1,44 @@
 function redirectShorts(details) {
     chrome.storage.local.get('blockingEnabled', data => {
         if (data.blockingEnabled && details.url.includes('/shorts/')) {
-            chrome.tabs.update(details.tabId, { url: "https://www.youtube.com/" });
+            chrome.tabs.update(details.tabId, { url: "https://www.youtube.com/" }, () => {
+                if(chrome.runtime.lastError) {
+                    console.log(`Error redirecting ${chrome.runtime.lastError}`);
+                } else {
+                    chrome.notifications.create({
+                        type: 'basic',
+                        iconUrl: '128x128.png',  // Ensure you have an icon.png in your extension directory
+                        title: 'Redirection Notice',
+                        message: 'You were redirected away from YouTube Shorts. Now, go be productive :)',
+                        priority: 2
+                    });
+                }
+            });
         }
     });
 }
 function redirectShortsOnActiveChange(activeInfo) {
     chrome.storage.local.get('blockingEnabled', data => {
-        chrome.tabs.get(activeInfo.tabId).then(currentTab => {
+        chrome.tabs.get(activeInfo.tabId, currentTab => {
+            console.log("currentTab", currentTab);
+            if (chrome.runtime.lastError) {
+                console.error(`Error retrieving tab: ${chrome.runtime.lastError}`);
+                return;
+            }
             if (data.blockingEnabled && currentTab.url.includes('/shorts/')) {
-                chrome.tabs.update(activeInfo.tabId, { url: "https://www.youtube.com/" });
+                chrome.tabs.update(activeInfo.tabId, { url: "https://www.youtube.com/" }, () => {
+                    if (chrome.runtime.lastError) {
+                        console.error(`Error redirecting: ${chrome.runtime.lastError}`);
+                    } else {
+                        chrome.notifications.create({
+                            type: 'basic',
+                            iconUrl: '128x128.png',  // Ensure you have an icon.png in your extension directory
+                            title: 'Redirection Notice',
+                            message: 'You were redirected away from YouTube Shorts. Now, go be productive :)',
+                            priority: 2
+                        });
+                    }
+                });
             }
         });
     });
